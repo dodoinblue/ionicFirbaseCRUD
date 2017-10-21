f615174d1f26a6bd2ff729ccf239927d32e403de
Charles <dodoinblue@gmail.com>
2017-10-21 11:32:19 -0700

    install firebase dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
+import { AngularFireModule } from 'angularfire2';
+import { FIREBASE_CREDENTIALS } from './firebase.credentials';
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
@@ -14,7 +17,10 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+
+    // Init angular
+    AngularFireModule.initializeApp(FIREBASE_CREDENTIALS)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+  apiKey: "AIzaSyBEgdIPNvdUQRbwsnrAxJnMlyQ9j0XJuJg",
+  authDomain: "fir-crud-fa4a3.firebaseapp.com",
+  databaseURL: "https://fir-crud-fa4a3.firebaseio.com",
+  projectId: "fir-crud-fa4a3",
+  storageBucket: "fir-crud-fa4a3.appspot.com",
+  messagingSenderId: "877839762051"
+}
